6260210100abc8ae1380fc7918698f1a71fa04dc
Chris <chrisdniels12@gmail.com>
2025-11-11 06:31:52 +0000

    Add request logging middleware using morgan

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,22 @@ import bankAccountRoutes from "./routes/bankAccountRoutes.js"
 import userAuthRoutes from "./routes/userAuthRoutes.js"
 import transactionRoutes from "./routes/transactionRoutes.js"
 import pool from "./db.js";
+import morgan from "morgan";
 
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 8000;
+//  format string - log method, route, status, size, and response time
+const format = ":method :url :status :res[content-length] - :response-time ms";
 
+//  detailed logs in dev
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan(format));
+} else {
+  // Use 'tiny' logs in production,
+  app.use(morgan("tiny"));
+}
 app.use(cors({
   origin: process.env.REACT_CLIENT_URL || "http://localhost:5173",
   credentials: true,
